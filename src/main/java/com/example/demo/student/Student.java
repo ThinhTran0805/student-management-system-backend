9fa9b9bdff94145d03eaaa711e7b0dd9bbf8c645
ThinhTran <15520847@gm.uit.edu.vn>
2026-05-08 23:12:24 +0700

    feat: implement Student Entity and Data Access with JPA
    - Define Student Entity with @Id, @SequenceGenerator and @Transient for age calculation.
    - Create StudentRepository interface extending JpaRepository.
    - Implement Data Seeding in StudentConfig using CommandLineRunner.
    - Setup StudentController and StudentService for API endpoints.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,9 +3,11 @@ package com.example.demo.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
+
 public class Student {
     @Id
     @SequenceGenerator(
@@ -21,25 +23,29 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
-    private int age;
+
+    @Transient
+    private Integer age;
+
+    public Student() {
+
+    }
 
     public Student(long id) {
         this.id = id;
     }
 
-    public Student(long id, String name, String email, LocalDate dob, int age) {
+    public Student(long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, int age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public long getId() {
@@ -74,11 +80,11 @@ public class Student {
         this.dob = dob;
     }
 
-    public int getAge() {
-        return age;
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
