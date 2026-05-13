722f0a3a68a5229e86a2b2584386de8293831066
ThinhTran <15520847@gm.uit.edu.vn>
2026-05-13 17:15:19 +0700

    feat: add POST/DELETE student with email validation (2026-05-13)
    Implement business logic to check if email exists before adding new student.
    
    Add DELETE endpoint with student existence check.
    
    Configure server to include error messages in response.
    
    Handled business logic in StudentService to ensure data integrity.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,8 +2,12 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.sql.Struct;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -17,5 +21,21 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exist = studentRepository.existsById(studentId);
+        if (!exist) {
+            throw new IllegalStateException("Student with " + studentId + " does not exist!");
+        }
+        studentRepository.deleteById(studentId);
     }
 }
